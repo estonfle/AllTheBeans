2585546c7855a7096e91866f44f0dfce7c4537a2
estonfle <githubprimary.boxcar138@passmail.net>
2025-11-22 17:37:41 +0000

    Add login and registration pages with navigation links in the layout

--- AllTheBeans-Frontend/src/App.tsx
+++ AllTheBeans-Frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 export default function App() {
   return (
@@ -8,6 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- AllTheBeans-Frontend/src/components/Layout.tsx
+++ AllTheBeans-Frontend/src/components/Layout.tsx
@@ -1,12 +1,19 @@
-import { AppBar, Toolbar, Container } from '@mui/material';
-import { Outlet } from 'react-router-dom';
+import { AppBar, Toolbar, Container, Typography, Button } from '@mui/material';
+import { Outlet, Link as RouterLink } from 'react-router-dom';
+import LocalCafeIcon from '@mui/icons-material/LocalCafe';
 
 export default function Layout() {
     return (
         <>
             <AppBar position="sticky">
                 <Toolbar>
+                    <LocalCafeIcon sx={{ mr: 2 }} />
+                    <Typography variant="h6" sx={{ flexGrow: 1, textDecoration: 'none', color: 'inherit' }} component={RouterLink} to="/">
+                        All The Beans
+                    </Typography>
 
+                    <Button color="inherit" component={RouterLink} to="/login">Login</Button>
+                    <Button color="inherit" component={RouterLink} to="/register">Register</Button>
                 </Toolbar>
             </AppBar>
 
